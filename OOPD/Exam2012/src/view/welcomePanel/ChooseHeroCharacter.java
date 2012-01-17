ed9fc3cb1ad3281964c5df7c0f446f263fbdd4a4
Jonas Brunsgaard <jonas.brunsgaard@gmail.com>
2012-01-17 18:29:28 +0100

    coding style submittet

--- OOPD/Exam2012/src/view/welcomePanel/ChooseHeroCharacter.java
+++ OOPD/Exam2012/src/view/welcomePanel/ChooseHeroCharacter.java
@@ -1,8 +1,20 @@
 package view.welcomePanel;
 
 import javax.swing.JPanel;
+import java.awt.GridLayout;
 
 public class ChooseHeroCharacter extends JPanel
 {
+    private JPanel buttonPane;
+
+    public ChooseHeroCharacter(){
+        
+        setLayout(new GridLayout(1, 2));
+        
+        this.buttonPane = new JPanel();
+        this.buttonPane.setLayout(new GridLayout(3, 1));
+        
+        
+    }
 
 }
