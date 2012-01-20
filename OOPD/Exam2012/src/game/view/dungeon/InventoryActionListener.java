5e285d9baeb781b09c63caa694f4ee5bdce5032f
Hans Duedal <hd@onlinecity.dk>
2012-01-20 19:04:47 +0100

    Working potions and weapons

--- OOPD/Exam2012/src/game/view/dungeon/InventoryActionListener.java
+++ OOPD/Exam2012/src/game/view/dungeon/InventoryActionListener.java
@@ -1,23 +1,38 @@
 package game.view.dungeon;
 
+import game.model.Dungeon;
 import game.model.items.Item;
+import game.model.items.Potion;
+import game.model.items.Weapon;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
+import javax.swing.RepaintManager;
+
 public class InventoryActionListener implements ActionListener 
 {
     private Item item;
-    public InventoryActionListener(Item item)
+    private InventoryPanel panel;
+    
+    public InventoryActionListener(Item item, InventoryPanel panel)
     {
         super();
         this.item = item;
+        this.panel = panel;
     }
 
     @Override
     public void actionPerformed(ActionEvent e)
     {
-        System.out.println("Hero select: "+this.item); //TODO
-        
+        if (item instanceof Potion) {
+            JButton button = (JButton) e.getSource();
+            Dungeon.getInstance().getHero().drinkPotion((Potion) item);
+            panel.remove(button);
+            RepaintManager.currentManager(panel).markCompletelyDirty(panel);
+        }
+        if (item instanceof Weapon)
+            Dungeon.getInstance().getHero().selectWeapon((Weapon) item);
     }
 }
